8b87dd5172be762ecd802e3397ba26424722b41d
Jake Soenneker <jake@soenneker.com>
2024-12-20 11:02:14 -0600

    Tests

--- test/Soenneker.Extensions.DateTime.Year.Tests/DateTimeYearExtensionTests.cs
+++ test/Soenneker.Extensions.DateTime.Year.Tests/DateTimeYearExtensionTests.cs
@@ -1,5 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
 namespace Soenneker.Extensions.DateTime.Year.Tests;
 
-public class DateTimeYearExtensionTests
+public class DateTimeYearExtensionTests : UnitTest
 {
+    [Fact]
+    public void Default()
+    {
+
+    }
 }
