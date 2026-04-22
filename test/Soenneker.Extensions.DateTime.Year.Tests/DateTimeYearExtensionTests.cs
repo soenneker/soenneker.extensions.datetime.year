e35a6b16ed1ec2c78a180b1cc7b0691b264e8b92
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:52 -0500

    TUnit conversion

--- test/Soenneker.Extensions.DateTime.Year.Tests/DateTimeYearExtensionTests.cs
+++ test/Soenneker.Extensions.DateTime.Year.Tests/DateTimeYearExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.DateTime.Year.Tests;
 
 public class DateTimeYearExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
